e85e6fc99eb16ae62695e86a159caf07fda42fbf
hamazin <hademolahamazin@gmail.com>
2025-08-17 07:44:41 +0100

    Intro to database

---
+++ alx_book_store.sql
@@ -0,0 +1,32 @@
+CREATE Books (
+    book_id PRIMARY number,
+    title VARCHAR(130),
+    author_id ,
+    price DOUBLE,
+    publication_date DATE
+)
+
+CREATE Authors(
+    author_id PRIMARY,
+    author_name VARCHAR(215)
+)
+
+CREATE Customers(
+    customer_id PRIMARY,
+    customer_name VARCHAR(215)
+    email VARCHAR(215)
+    address TEXT
+)
+
+CREATE Orders(
+    order_id primary,
+    customer_id,
+    order_date DATE
+)
+
+CREATE Order(
+    orderdetailid PRIMARY,
+    order_id
+    book_id
+    quantity DOUBLE
+)
